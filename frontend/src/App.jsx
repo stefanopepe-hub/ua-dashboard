9c888800ce37bd71ac497d9a024cc198a4a9ef58
stefanopepe-hub <stefanopepe@me.com>
2026-04-10 21:35:17 +0200

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,22 @@ import Saving from './pages/Saving'
 import Tempi from './pages/Tempi'
 import NonConformita from './pages/NonConformita'
 import Fornitori from './pages/Fornitori'
+import AlfaDoc from './pages/AlfaDoc'
 import Upload from './pages/Upload'
+import DataQuality from './pages/DataQuality'
 
 export default function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Riepilogo />} />
-        <Route path="/saving" element={<Saving />} />
-        <Route path="/tempi" element={<Tempi />} />
-        <Route path="/nc" element={<NonConformita />} />
+        <Route path="/"          element={<Riepilogo />} />
+        <Route path="/saving"    element={<Saving />} />
+        <Route path="/tempi"     element={<Tempi />} />
+        <Route path="/nc"        element={<NonConformita />} />
         <Route path="/fornitori" element={<Fornitori />} />
-        <Route path="/upload" element={<Upload />} />
+        <Route path="/alfa-doc"  element={<AlfaDoc />} />
+        <Route path="/upload"    element={<Upload />} />
+        <Route path="/quality"   element={<DataQuality />} />
       </Routes>
     </Layout>
   )
