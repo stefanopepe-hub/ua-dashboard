318b3b4e86025be18a37ffd65dbe6d609b241185
stefanopepe-hub <stefanopepe@me.com>
2026-04-11 22:07:38 +0200

    Add column synonyms for various fields

---
+++ enterprise_v2/backend/column_synonyms.py
@@ -0,0 +1,70 @@
+COLUMN_SYNONYMS = {
+    "supplier_name": [
+        "fornitore",
+        "ragione sociale",
+        "ragione sociale fornitore",
+        "supplier",
+        "supplier name",
+        "vendor",
+        "vendor name",
+    ],
+    "document_type": [
+        "tipo documento",
+        "codice documento",
+        "tipo doc",
+        "document type",
+        "doc type",
+        "alfa documento",
+    ],
+    "document_date": [
+        "data documento",
+        "data doc",
+        "document date",
+        "doc date",
+    ],
+    "list_amount": [
+        "importo listino",
+        "listino",
+        "imp iniziale",
+        "imp. iniziale",
+        "initial amount",
+        "list amount",
+    ],
+    "committed_amount": [
+        "impegnato",
+        "importo impegnato",
+        "committed amount",
+        "amount committed",
+    ],
+    "saving_amount": [
+        "saving",
+        "risparmio",
+        "saving amount",
+        "saved amount",
+    ],
+    "cost_center": [
+        "cdc",
+        "centro di costo",
+        "cost center",
+    ],
+    "buyer_name": [
+        "utente",
+        "buyer",
+        "responsabile",
+        "purchaser",
+    ],
+    "protocol_commessa": [
+        "protocollo commessa",
+        "commessa",
+        "project protocol",
+    ],
+    "protocol_order": [
+        "protocollo ordine",
+        "order protocol",
+    ],
+    "resource_name": [
+        "risorsa",
+        "resource",
+        "team member",
+    ],
+}
