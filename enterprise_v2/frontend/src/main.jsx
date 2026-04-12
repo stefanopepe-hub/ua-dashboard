e40c0dca3f1534f175f49268a0d2fd3cc3e22061
stefanopepe-hub <stefanopepe@me.com>
2026-04-12 08:44:55 +0200

    Add files via upload

--- enterprise_v2/frontend/src/main.jsx
+++ enterprise_v2/frontend/src/main.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div style={{ fontFamily: "Arial, sans-serif", padding: "24px" }}>
-      <h1>Telethon Enterprise V2</h1>
-      <p>Nuova piattaforma enterprise separata dall'app attuale.</p>
-    </div>
-  );
-}
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
